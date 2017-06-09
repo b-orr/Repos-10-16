5f8023406e68eb2a20dd7b385edf4319934d8a99
Trajche <tmichov@rubixcubesolutions.com>
2017-06-09 17:08:15 +0200

    projections fix

--- erp/Projections/includes/_footer.php
+++ erp/Projections/includes/_footer.php
@@ -46,20 +46,20 @@
 		<!--================================================== -->
 
 		<!-- PACE LOADER - turn this on if you want ajax loading to show (caution: uses lots of memory on iDevices)-->
-		<script data-pace-options='{ "restartOnRequestAfter": true }' src="../../assets/js/plugin/pace/pace.min.js"></script>
+		<script data-pace-options='{ "restartOnRequestAfter": true }' src="../../../assets/js/plugin/pace/pace.min.js"></script>
 
 		<!-- Link to Google CDN's jQuery + jQueryUI; fall back to local -->
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script>
 			if (!window.jQuery) {
-				document.write('<script src="../../assets/js/libs/jquery-2.1.1.min.js"><\/script>');
+				document.write('<script src="../../../assets/js/libs/jquery-2.1.1.min.js"><\/script>');
 			}
 		</script>
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<script>
 			if (!window.jQuery.ui) {
-				document.write('<script src="../../assets/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
+				document.write('<script src="../../../assets/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
 			}
 		</script>
 
@@ -70,55 +70,55 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script>
 			if (!window.jQuery) {
-				document.write('<script src="../../assets/js/libs/jquery-2.1.1.min.js"><\/script>');
+				document.write('<script src="../../../assets/js/libs/jquery-2.1.1.min.js"><\/script>');
 			}
 		</script>
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 		<script>
 			if (!window.jQuery.ui) {
-				document.write('<script src="../../assets/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
+				document.write('<script src="../../../assets/js/libs/jquery-ui-1.10.3.min.js"><\/script>');
 			}
 		</script>
 
 		<!-- IMPORTANT: APP CONFIG -->
-		<script src="../../assets/js/app.config.js"></script>
+		<script src="../../../assets/js/app.config.js"></script>
 
 		<!-- JS TOUCH : include this plugin for mobile drag / drop touch events-->
-		<script src="../../assets/js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script> 
+		<script src="../../../assets/js/plugin/jquery-touch/jquery.ui.touch-punch.min.js"></script> 
 
 		<!-- BOOTSTRAP JS -->
-		<script src="../../assets/js/bootstrap/bootstrap.min.js"></script>
+		<script src="../../../assets/js/bootstrap/bootstrap.min.js"></script>
 
 		<!-- CUSTOM NOTIFICATION -->
-		<script src="../../assets/js/notification/SmartNotification.min.js"></script>
+		<script src="../../../assets/js/notification/SmartNotification.min.js"></script>
 
 		<!-- JARVIS WIDGETS -->
-		<script src="../../assets/js/smartwidgets/jarvis.widget.min.js"></script>
+		<script src="../../../assets/js/smartwidgets/jarvis.widget.min.js"></script>
 
 		<!-- EASY PIE CHARTS -->
-		<script src="../../assets/js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
+		<script src="../../../assets/js/plugin/easy-pie-chart/jquery.easy-pie-chart.min.js"></script>
 
 		<!-- SPARKLINES -->
-		<script src="../../assets/js/plugin/sparkline/jquery.sparkline.min.js"></script>
+		<script src="../../../assets/js/plugin/sparkline/jquery.sparkline.min.js"></script>
 
 		<!-- JQUERY VALIDATE -->
-		<script src="../../assets/js/plugin/jquery-validate/jquery.validate.min.js"></script>
+		<script src="../../../assets/js/plugin/jquery-validate/jquery.validate.min.js"></script>
 
 		<!-- JQUERY MASKED INPUT -->
-		<script src="../../assets/js/plugin/masked-input/jquery.maskedinput.min.js"></script>
+		<script src="../../../assets/js/plugin/masked-input/jquery.maskedinput.min.js"></script>
 
 		<!-- JQUERY SELECT2 INPUT -->
-		<script src="../../assets/js/plugin/select2/select2.min.js"></script>
+		<script src="../../../assets/js/plugin/select2/select2.min.js"></script>
 
 		<!-- JQUERY UI + Bootstrap Slider -->
-		<script src="../../assets/js/plugin/bootstrap-slider/bootstrap-slider.min.js"></script>
+		<script src="../../../assets/js/plugin/bootstrap-slider/bootstrap-slider.min.js"></script>
 
 		<!-- browser msie issue fix -->
-		<script src="../../assets/js/plugin/msie-fix/jquery.mb.browser.min.js"></script>
+		<script src="../../../assets/js/plugin/msie-fix/jquery.mb.browser.min.js"></script>
 
 		<!-- FastClick: For mobile devices -->
-		<script src="../../assets/js/plugin/fastclick/fastclick.min.js"></script>
+		<script src="../../../assets/js/plugin/fastclick/fastclick.min.js"></script>
 
 		<!--[if IE 8]>
 
@@ -128,21 +128,21 @@
 
 
 		<!-- MAIN APP JS FILE -->
-		<script src="../../assets/js/app.min.js"></script>
+		<script src="../../../assets/js/app.min.js"></script>
 
 		<!-- ENHANCEMENT PLUGINS : NOT A REQUIREMENT -->
 		<!-- Voice command : plugin -->
-		<script src="../../assets/js/speech/voicecommand.min.js"></script>
+		<script src="../../../assets/js/speech/voicecommand.min.js"></script>
 
 		<!-- SmartChat UI : plugin -->
-		<script src="../../assets/js/smart-chat-ui/smart.chat.ui.min.js"></script>
-		<script src="../../assets/js/smart-chat-ui/smart.chat.manager.min.js"></script>
+		<script src="../../../assets/js/smart-chat-ui/smart.chat.ui.min.js"></script>
+		<script src="../../../assets/js/smart-chat-ui/smart.chat.manager.min.js"></script>
 
 		<!-- PAGE RELATED PLUGIN(S) -->
-		<script src="../../assets/js/plugin/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+		<script src="../../../assets/js/plugin/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
 
-		<script src="../../assets/js/plugin/datatables/jquery.dataTables.min.js"></script>
-		<script src="../../assets/js/plugin/datatables/dataTables.colVis.min.js"></script>
-		<script src="../../assets/js/plugin/datatables/dataTables.tableTools.min.js"></script>
-		<script src="../../assets/js/plugin/datatables/dataTables.bootstrap.min.js"></script>
-		<script src="../../assets/js/plugin/datatable-responsive/datatables.responsive.min.js"></script>
+		<script src="../../../assets/js/plugin/datatables/jquery.dataTables.min.js"></script>
+		<script src="../../../assets/js/plugin/datatables/dataTables.colVis.min.js"></script>
+		<script src="../../../assets/js/plugin/datatables/dataTables.tableTools.min.js"></script>
+		<script src="../../../assets/js/plugin/datatables/dataTables.bootstrap.min.js"></script>
+		<script src="../../../assets/js/plugin/datatable-responsive/datatables.responsive.min.js"></script>
