ce61f5e62d5bb3939151f8de0f3560d22685c671
Klime Grceski <klime.grceski@gmail.com>
2017-08-04 13:27:42 +0200

    s3 upload asynchrone

--- laravel/resources/views/drawings/drawingList.blade.php
+++ laravel/resources/views/drawings/drawingList.blade.php
@@ -1,5 +1,6 @@
 @include('../includes/_header')
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.14.1/js/jquery.fileupload.min.js"></script>
 		<!-- Left panel : Navigation area -->
 		<!-- Note: This width of the aside area can be adjusted through LESS variables -->
 		
@@ -305,12 +306,17 @@
 												<section>
 												<div class="form-group">
 													<label class="input" style="font-weight: bold;">File input</label><small class="font-xs"> (Click below to open File Explorer)</small>
-													<form action="{{ url('/project/file/upload')}}" 
+													<form action="<?php echo $upload->getFormUrl(); ?>"
+													      method="POST"
+													      enctype="multipart/form-data" 
 														class="dropzone" 
 														id="mydropzone">
-														<input type="hidden" value="" id="releaseDescription_hidden" name ="releaseDesc_hidden">
-														<input type="hidden" value="" id="releaseDate_hidden" name ="releaseDate_hidden">
-														{{ csrf_field() }}
+														
+														 <?php echo $upload->getFormInputsAsHtml(); ?>
+						 
+														
+														
+												 
 													</form> <!-- {{ asset('public/assets/plugins/upload.php') }} -->
 												</div>
 												</section>
@@ -728,6 +734,76 @@
 			})();
 
 		</script>
+		
+		
+		 <script>
+        $(document).ready(function () {
+
+            // Assigned to variable for later use.
+            var form = $('.direct-upload');
+            var filesUploaded = [];
+
+            // Place any uploads within the descending folders
+            // so ['test1', 'test2'] would become /test1/test2/filename
+            var folders = [];
+
+            form.fileupload({
+                url: form.attr('action'),
+                type: form.attr('method'),
+                datatype: 'xml',
+                add: function (event, data) {
+
+                    // Show warning message if your leaving the page during an upload.
+                    window.onbeforeunload = function () {
+                        return 'You have unsaved changes.';
+                    };
+
+                    // Give the file which is being uploaded it's current content-type (It doesn't retain it otherwise)
+                    // and give it a unique name (so it won't overwrite anything already on s3).
+                    var file = data.files[0];
+                    var filename = Date.now() + '.' + file.name.split('.').pop();
+                    form.find('input[name="Content-Type"]').val(file.type);
+                    form.find('input[name="key"]').val((folders.length ? folders.join('/') + '/' : '') + filename);
+
+                    // Actually submit to form to S3.
+                    data.submit();
+
+                    // Show the progress bar
+                    // Uses the file size as a unique identifier
+                    var bar = $('<div class="progress" data-mod="'+file.size+'"><div class="bar"></div></div>');
+                    $('.progress-bar-area').append(bar);
+                    bar.slideDown('fast');
+                },
+                progress: function (e, data) {
+                    // This is what makes everything really cool, thanks to that callback
+                    // you can now update the progress bar based on the upload progress.
+                    var percent = Math.round((data.loaded / data.total) * 100);
+                    $('.progress[data-mod="'+data.files[0].size+'"] .bar').css('width', percent + '%').html(percent+'%');
+                },
+                fail: function (e, data) {
+                    // Remove the 'unsaved changes' message.
+                    window.onbeforeunload = null;
+                    $('.progress[data-mod="'+data.files[0].size+'"] .bar').css('width', '100%').addClass('red').html('');
+                },
+                done: function (event, data) {
+                    window.onbeforeunload = null;
+
+                    // Upload Complete, show information about the upload in a textarea
+                    // from here you can do what you want as the file is on S3
+                    // e.g. save reference to your server using another ajax call or log it, etc.
+                    var original = data.files[0];
+                    var s3Result = data.result.documentElement.children;
+                    filesUploaded.push({
+                        "original_name": original.name,
+                        "s3_name": s3Result[2].innerHTML,
+                        "size": original.size,
+                        "url": s3Result[0].innerHTML.replace("%2F", "/")
+                    });
+                    $('#uploaded').html(JSON.stringify(filesUploaded, null, 2));
+                }
+            });
+        });
+    </script>
 
 	</body>
 
