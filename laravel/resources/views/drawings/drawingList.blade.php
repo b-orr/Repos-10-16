be0dc56c0c04606bb7cbd1fd5dad9d069b44fad0
Klime Grceski <klime.grceski@gmail.com>
2017-08-29 13:47:57 +0200

    no message

--- laravel/resources/views/drawings/drawingList.blade.php
+++ laravel/resources/views/drawings/drawingList.blade.php
@@ -331,9 +331,9 @@
 										<button type="button" class="btn btn-default" data-dismiss="modal">
 											Cancel
 										</button>
-										<button type="button" class="btn btn-primary" data-dismiss="modal" id="pdfUpload" >
+									<!--	<button type="button" class="btn btn-primary" data-dismiss="modal" id="pdfUpload" >
 											Upload
-										</button>
+										</button>-->
 									</div>
 							</div><!-- /.modal-content -->
 						</div><!-- /.modal-dialog -->
