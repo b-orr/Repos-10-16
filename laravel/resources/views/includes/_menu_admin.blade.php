5e088fdbace18cb02291a549865c6569df11583b
Trajche <tmichov@rubixcubesolutions.com>
2017-09-15 17:18:03 +0200

    admin menu update

--- laravel/resources/views/includes/_menu_admin.blade.php
+++ laravel/resources/views/includes/_menu_admin.blade.php
@@ -8,19 +8,39 @@
 				-->
 
 				<ul class="main-Menu">
-					<li>
+					<li  
+						@if((strpos(Request::url(),'admin/company') !== false))
+						class="active" 
+						@endif
+					>
 						<a href="{{ url('admin/company') }}"><i class="fa fa-lg fa-fw fa-list-alt" style="color: #404040;"></i> <span class="menu-item-parent">Company Information</span></a>
 					</li>
-					<li class="active">
+					<li
+						@if((strpos(Request::url(),'admin/regions') !== false))
+						class="active" 
+						@endif
+					>
 						<a href="{{ url('admin/regions') }}"><i class="fa fa-lg fa-fw fa-list-alt" style="color: #404040;"></i> <span class="menu-item-parent">Regions</span></a>
 					</li>
-					<li>
+					<li
+						@if((strpos(Request::url(),'admin/fields') !== false))
+						class="active" 
+						@endif
+					>
 						<a href="{{ url('admin/fields') }}"><i class="fa fa-lg fa-fw fa-list-alt" style="color: #404040;"></i> <span class="menu-item-parent">Fields Manager</span></a>
 					</li>
-					<li>
+					<li
+						@if((strpos(Request::url(),'admin/user_groups') !== false))
+						class="active" 
+						@endif
+					>
 						<a href="{{ url('admin/user_groups') }}"><i class="fa fa-lg fa-fw fa-list-alt" style="color: #404040;"></i> <span class="menu-item-parent">User Groups</span></a>
 					</li>
-					<li>
+					<li
+						@if((strpos(Request::url(),'admin/users') !== false))
+						class="active" 
+						@endif
+					>
 						<a href="{{ url('admin/users') }}"><i class="fa fa-lg fa-fw fa-pencil" style="color: #404040;"></i> <span class="menu-item-parent">Users</span></a>
 					</li>
 				</ul>
