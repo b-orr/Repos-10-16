1df7fff9952ab7f92454a91e29dac04136994c3b
Trajche <tmichov@rubixcubesolutions.com>
2017-10-04 15:21:24 +0200

    company and locations database update

---
+++ laravel/database/migrations/2017_10_04_130500_con_company_locations.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConCompanyLocations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('con_company_locations', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('company_id');
+            $table->string('location_name');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->index('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('con_company_locations');
+    }
+}
